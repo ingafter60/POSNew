2d659c3d479683a9700c46999278888ef4ba3514
ing <inyoman_gurnitha@yahoo.com>
2020-01-16 10:17:01 +0700

    43. Configure header footer file in dashboard-php

---
+++ inc/footer.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+    <!-- codes deleted -->
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+  immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 3 -->
+<script src="assets/jquery/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/AdminLTE/js/adminlte.min.js"></script><!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
